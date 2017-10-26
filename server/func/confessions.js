d7612789e243b45799d0c56d9c1d561b3f798ff5
Edgar Khanzadian <edgar.khanzadyan@yandex.ru>
2017-10-26 17:03:51 +0400

    change send_confession function to one that is on aws

--- server/func/confessions.js
+++ server/func/confessions.js
@@ -13,32 +13,28 @@ module.exports.process_conf = (event, context, callback) => {
     const {some_thing} = data;
 };
 module.exports.send_confession = (event, context, callback) => {
+    console.log(event.body);
     const data = JSON.parse(event.body);
-    const {some_thing} = data;
-    if("ERROR") {
-        context.failed(new Error(err));
+    if(data) {
+        const {confession} = data;
+        console.log(confession);
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: 'Sent to fb group',
+                confession
+                // input: event
+            })
+        };
+        context.succeed(response);
     } else {
-        if("AI approves") {
-            console.log("send to FB group");
-            const response = {
-                statusCode: 200,
-                body: JSON.stringify({
-                    message: 'Sent to fb group',
-                    // input: event
-                })
-            };
-            context.succeed(response);
-        } else {
-            console.log("send to admin's email");
-            const response = {
-                statusCode: 200,
-                body: JSON.stringify({
-                    message: 'Sent to admin',
-                    // input: event
-                })
-            };
-            context.succeed(response);
-        }
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: 'Didn\'t work'
+            })
+        };
+        context.succeed(response);
     }
 };
 module.exports.send_feedback = (event, context, callback) => {
