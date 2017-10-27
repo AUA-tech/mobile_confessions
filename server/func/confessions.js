1f4fdcf9d7f65f3fa8c64e013e0b7d28b7c61070
Edgar Khanzadian <edgar.khanzadyan@yandex.ru>
2017-10-27 08:02:05 +0400

    send fb token

--- server/func/confessions.js
+++ server/func/confessions.js
@@ -1,7 +1,7 @@
 const AWS = require('aws-sdk');
 const moment = require('moment');
 const uuid = require('uuid');
-
+const FB = require('fb');
 //const SES = new AWS.SES();
 
 module.exports.get_confession = (event, context, callback) => {
@@ -37,6 +37,43 @@ module.exports.send_confession = (event, context, callback) => {
         context.succeed(response);
     }
 };
+module.exports.get_access_token = (event, context, callback) => {
+
+    FB.api('oauth/access_token', {
+        client_id: process.env.FB_APP_ID,
+        client_secret: process.env.FB_APP_SECRET,
+        grant_type: 'client_credentials'
+    }, res => {
+        if (!res || res.error) {
+            
+            const error_message = !res ? 'error occurred during the token request' : res.error;
+            const response = {
+                statusCode: 200,
+                body: JSON.stringify({
+                    message: 'Didnt work'
+                })
+            };
+            context.succeed(response);
+            // context.failed(new Error(error_message));
+            console.log(error_message);
+            return;
+        }
+        const {
+            access_token
+        } = res;
+
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: 'Success',
+                access_token
+            })
+        };
+        context.succeed(response);
+        // FB.setAccessToken(access_token);
+    });
+
+}
 module.exports.send_feedback = (event, context, callback) => {
     const data = JSON.parse(event.body);
     const {some_thing} = data;
