d133a61b097e34579ede793b8fb7cf9e52423481
Edgar Khanzadian <edgar.khanzadyan@yandex.ru>
2017-11-06 16:07:59 +0400

    change confessionCard style: keep image ratio, adjust spacing

--- mobile/lib/components/confessionCard.js
+++ mobile/lib/components/confessionCard.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import styled from 'styled-components/native';
-import { Text, Image } from 'react-native';
+import { Text, Image, View, Dimensions } from 'react-native';
 import moment from 'moment';
 
 import colors from '../constants/colors';
+const {height, width} = Dimensions.get('window');
 
 const ConfessionCard = ({
   link,
@@ -13,20 +14,27 @@ const ConfessionCard = ({
   attachments,
   created_time
 }) => {
+  // If image attachment exists, get it's ratio
+  const image_height = attachments ? attachments.data[0].media.image.height : undefined;
+  const image_width = attachments ? attachments.data[0].media.image.width : undefined;
+  const ratio = image_width / image_height;
+
   const confessionNumberMatch = message.match(/#([0-9]+)\d/); // Match a regexp for extracting confession number
   const confessionNumber = confessionNumberMatch ? confessionNumberMatch[0] : ''; // Match data is an array so we validate and take the first item if valid
   const clearedMessage = confessionNumberMatch ? message.split(confessionNumber)[1]  : message; // We also need to clear the message if the match is not null
   return (
     <CardView>
-      <RowView>
-        <NumberText>{confessionNumber}</NumberText>
-        <DateText>{moment(created_time).format('MMMM Do YYYY, HH:MM')}</DateText>
-      </RowView>
-      <Text>{clearedMessage}</Text>
+      <TextContentView>
+        <RowView>
+          <NumberText>{confessionNumber}</NumberText>
+          <DateText>{moment(created_time).format('MMM D, HH:MM')}</DateText>
+        </RowView>
+        <Text>{clearedMessage}</Text>
+      </TextContentView>
       { !full_picture ? null :
         <Image
-          style={{width: '100%', height: 250}}
-          source={{uri: full_picture}}
+          style={{width, height: width / ratio}}
+          source={{uri: attachments.data[0].media.image.src}}
         />
       }
     </CardView>
@@ -50,11 +58,14 @@ const RowView = styled.View`
   justify-content: space-between;
 `
 
-const CardView = styled.View`
+const TextContentView = styled.View`
   padding-horizontal: 10;
   padding-vertical: 10;
+`
+const CardView = styled.View`
   background-color: white;
   margin-bottom: 5;
+  padding-bottom: 5;
 `
 
 export default ConfessionCard;
