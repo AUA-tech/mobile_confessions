bb6453c60deca7e2af7bcc25a2f89b4f6486a652
Edgar Khanzadian <edgar.khanzadyan@yandex.ru>
2017-11-06 19:27:51 +0400

    change zIndex of animation

--- mobile/lib/components/sendNotification.js
+++ mobile/lib/components/sendNotification.js
@@ -7,7 +7,9 @@ import { height, width } from '../constants/styles';
 export default class SendNotification extends PureComponent {
 
   notification_opacity = new Animated.Value(0);
+  notification_z_index = new Animated.Value(-500);
   show_notification_f = () => {
+    this.notification_z_index = new Animated.Value(500);
     Animated.sequence([
       Animated.timing(this.notification_opacity, {
         toValue: 1,
@@ -19,7 +21,8 @@ export default class SendNotification extends PureComponent {
         duration: 500
       })
     ]).start(() => {
-      this.props.done()
+      this.notification_z_index = new Animated.Value(-500);
+      this.props.done();
     })
   }
 
@@ -27,8 +30,15 @@ export default class SendNotification extends PureComponent {
     if(this.props.show_notification) {
       this.show_notification_f();
     }
+    const with_animations_style = [
+      notificationStyle, 
+      {
+        opacity: this.notification_opacity,
+        zIndex: this.notification_z_index
+      }
+    ];
     return(
-      <Animated.View style={[notificationStyle, {opacity: this.notification_opacity}]}>
+      <Animated.View style={with_animations_style}>
         <NotificationText>
           {this.props.message}
         </NotificationText>
