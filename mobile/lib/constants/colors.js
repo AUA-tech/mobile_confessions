72bc2195e26477dcc176bba24b034d81d880bca3
Robert Gevorgyan <robert1999.g@gmail.com>
2017-11-07 03:49:51 +0400

    Brighter the bacgkround color of the flat list

--- mobile/lib/constants/colors.js
+++ mobile/lib/constants/colors.js
@@ -2,7 +2,7 @@ const colors = {
   primaryColor: '#3A4F60',
   inactiveTintColor: '#BBC2C9',
   sucessColor: '#53D769',
-  bgColor: '#EAEAEA',
+  bgColor: '#EFEFEF',
   headerColor: '#4B4B4B',
   softTextColor: '#8E8E93',
   placeholderColor: '#BABABA',
