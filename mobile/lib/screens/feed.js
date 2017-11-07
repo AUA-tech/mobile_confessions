313cd7377bb6dda321a1b8c31f4e16afbda3754e
Edgar Khanzadian <edgar.khanzadyan@yandex.ru>
2017-11-07 04:14:01 +0400

    refactor hide, add report

--- mobile/lib/screens/feed.js
+++ mobile/lib/screens/feed.js
@@ -1,12 +1,15 @@
 import React, { PureComponent } from 'react';
-import { FlatList, AsyncStorage, ActivityIndicator } from 'react-native';
+import { FlatList, AsyncStorage, ActivityIndicator, View, Text, TextInput, Alert } from 'react-native';
 import { differenceBy } from 'lodash';
 import { GraphRequest, GraphRequestManager } from 'react-native-fbsdk';
+import Modal from 'react-native-modal';
+import ActionSheet from 'react-native-actionsheet'
 
 import Layout from '../components/layout';
 import TabIcon from '../components/tabIcon';
 import ConfessionCard from '../components/confessionCard';
 import colors from '../constants/colors';
+import {height, width} from '../constants/styles';
 import { awsGet } from '../utils';
 
 export default class ConfessionsFeedScreen extends PureComponent {
@@ -27,7 +30,9 @@ export default class ConfessionsFeedScreen extends PureComponent {
       confessionsList: [],
       accessToken: '',
       fetchStatus: 1,
-      hided_posts: []
+      hided_posts: [],
+      modalVisible: false,
+      actionsheet_selected_id: ''
     }
   }
   async componentDidMount(){
@@ -53,7 +58,10 @@ export default class ConfessionsFeedScreen extends PureComponent {
   createConfessionCards = ({ item: confession }) => (
     <ConfessionCard
       {...confession}
-      hide_post={(new_hided_post) => this.setState({hided_posts: [...this.state.hided_posts, new_hided_post]})}
+      open_action_sheet={(id) => {
+        this.setState({actionsheet_selected_id: id})
+        this.ActionSheet.show()
+      }}
     />
   );
 
@@ -100,7 +108,33 @@ export default class ConfessionsFeedScreen extends PureComponent {
       console.log(error);
     }
   }
+  handle_action_sheet (index) {
+      switch (index) {
+        case 0:
+          console.warn('Close ActionSheet');
+          break;
+        case 1:
+          Alert.alert(`Hide this post?`, '', [
+            {text: 'No', onPress: null, style: 'cancel'},
+            {text: 'Yes', onPress: async () => {
+              const hided_posts_str = await AsyncStorage.getItem('hided_posts');
+              const hided_posts = JSON.parse(hided_posts_str);
 
+              this.setState({hided_posts: [...this.state.hided_posts, this.state.actionsheet_selected_id]})
+
+              hided_posts.push(this.state.actionsheet_selected_id);
+              const updated_hided_posts_str = JSON.stringify(hided_posts);
+              AsyncStorage.setItem('hided_posts', updated_hided_posts_str);
+            }}
+          ]);
+          break;
+        case 2:
+          this.setState({modalVisible: true});
+          break;
+        default:
+          break;
+      }
+    }
   render() {
     const { confessionsList, accessToken, fetchStatus } = this.state;
     const filteredConfessionList = confessionsList.filter((post) => {
@@ -108,6 +142,29 @@ export default class ConfessionsFeedScreen extends PureComponent {
     })
     return (
       <Layout headerTitle='Feed'>
+        <Modal
+          style={{alignItems: 'center', justifyContent: 'center'}}
+          isVisible={this.state.modalVisible}
+          avoidKeyboard
+          onBackdropPress={() => this.setState({modalVisible: false})}
+        >
+          <View style={{height: width / 1.5, width: width / 1.5, backgroundColor: 'white'}}>
+            <Text>What's wrong with this post {this.state.actionsheet_selected_id}? Send report to AUA tech support!</Text>
+            <TextInput
+              style={{
+                height: '50%',
+                width: '100%',
+                fontSize: 20
+              }}
+              multiline
+              numberOfLines = {4}
+              onChangeText={this.onTextChange}
+              value={this.state.confession}
+              placeholder={'Write your report'}
+              placeholderTextColor={colors.placeholderColor}
+            />
+          </View>
+        </Modal>
         <FlatList
           key='scrollView'
           data={filteredConfessionList}
@@ -122,6 +179,13 @@ export default class ConfessionsFeedScreen extends PureComponent {
         { fetchStatus !== 1 ? null :
           <ActivityIndicator />
         }
+        <ActionSheet
+          ref={o => this.ActionSheet = o}
+          options={['Cancel', 'Hide', 'Report']}
+          cancelButtonIndex={0}
+          destructiveButtonIndex={2}
+          onPress={(i) => this.handle_action_sheet(i)}
+        />
       </Layout>
     );
   }
