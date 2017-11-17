8d93b0ac0cd07a5cc2ba045511f6562f86f3f42c
Edgar Khanzadian <edgar.khanzadyan@yandex.ru>
2017-11-17 12:06:00 +0400

    Show red header when device has no internet connection

--- mobile/lib/components/header.js
+++ mobile/lib/components/header.js
@@ -1,32 +1,84 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import styled from 'styled-components/native';
-import { StatusBar } from 'react-native';
+import { StatusBar, NetInfo, View, Text } from 'react-native';
 
 import colors from '../constants/colors';
 
-const Header = ({ title }) => (
-  <CenteredView>
-    <StatusBar
-      backgroundColor={colors.primaryColor}
-      barStyle="light-content"
-    />
-    <HeaderText>{title}</HeaderText>
-  </CenteredView>
-);
+class Header extends PureComponent {
+
+  state = {
+    is_connected: false,
+    which_connection: 'none'
+  }
+
+  componentDidMount() {
+    const handleFirstConnectivityChange = (reach) => {
+      switch (reach.type) {
+        case 'none':
+        case 'unknown':
+          this.setState({ which_connection: reach, is_connected: false })
+          break;
+        case 'wifi':
+        case 'cellular':
+          this.setState({ which_connection: reach, is_connected: true })
+          break;
+      }
+    }
+    NetInfo.addEventListener(
+      'connectionChange',
+      handleFirstConnectivityChange
+    );
+  }
+
+  componentWillUnmount() {
+    NetInfo.removeEventListener(
+      'connectionChange'
+    )
+  }
+
+  render() {
+    const { title } = this.props;
+    const { is_connected } = this.state;
+
+    return(
+      <CenteredView style={{backgroundColor: is_connected ? colors.primaryColor : colors.noInternetColor }}>
+        <StatusBar
+          backgroundColor={ is_connected ? colors.primaryColor : colors.noInternetColor }
+          barStyle="light-content"
+        />
+        <HeaderText>{title}</HeaderText>
+        {
+          is_connected ? null :
+          <View>
+            <NoInternetConnectionText>
+              no internet connection
+            </NoInternetConnectionText>
+          </View>
+        }
+      </CenteredView>
+    )
+  }
+
+}
 
 const CenteredView = styled.View`
   align-items: center;
   justify-content: center;
-  background-color: ${colors.primaryColor}
   height: 10%;
   min-height: 50;
 `
 
 const HeaderText = styled.Text`
   color: white;
-  font-size: 25;
+  font-size: 22;
   font-weight: 700;
   padding-top: 15;
+`
+
+const NoInternetConnectionText = styled.Text`
+  color: white;
+  font-size: 10;
+  opacity: 0.8;
 `
 
 export default Header;
