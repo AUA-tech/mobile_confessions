36ac4f88d8a50d9828023ded3dd87306d7359ccd
Edgar Khanzadian <edgar.khanzadyan@yandex.ru>
2017-12-03 02:23:10 +0400

    update codepush on resume

--- mobile/App.js
+++ mobile/App.js
@@ -147,4 +147,7 @@ MyApp.prototype.componentDidMount = () => {
 	});
 };
 
-export default codePush(MyApp);
+export default codePush({
+	checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
+	installMode: codePush.InstallMode.ON_NEXT_RESUME,
+})(MyApp);
